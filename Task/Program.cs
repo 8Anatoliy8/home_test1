0aec26dbe41180f93c6bdb14061f7d092b3b7958
Anatoliy Bogdevich <tolya.bogdevich@mail.ru>
2024-02-20 07:46:46 +0300

    some changed in task

--- Task/Program.cs
+++ Task/Program.cs
@@ -1,2 +1,25 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿string [] inputArray = {"Hello", "cat", "dog", "world", "1", "geekbrains", ":)"};
+string [] FilterArray (string [] array)
+{
+    int count = 0;
+    foreach (string str in array)
+    {
+        if (str.Length <=3)
+        {
+            count ++;
+        }
+    }
+    string [] resultArray = new string [count];
+    count = 0;
+    foreach (string str in array)
+    {
+        if (str.Length <=3)
+        {
+            resultArray[count] = str;
+            count++;
+        }
+    }
+    return resultArray;
+}
+string [] result = FilterArray (inputArray);
+Console.Write ($"Результат: {string.Join(", ", result)}");
